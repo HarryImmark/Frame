a78f0a51a3cf11a57a2694fde33e496a0577d598
Igor Nechay <dninotv@gmail.com>
2020-12-30 06:04:29 +0300

    Update commonConditions.java

--- src/test/java/Test/commonConditions.java
+++ src/test/java/Test/commonConditions.java
@@ -3,19 +3,21 @@ package Test;
 import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class commonConditions
 {
     protected WebDriver driver;
 
-    @BeforeMethod()
+    @BeforeTest()
     public void setUp()
     {
         driver = DriverSingleton.getDriver();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void stopBrowser()
     {
         DriverSingleton.closeDriver();
